48050c0ad368869d15c67d9a92ed1d442a7aad9e
VIPArcher <VIPArcher@sina.com>
2022-05-27 21:14:05 +0800

    重构脚本支持矩形拼接以及更多设置

--- scripts/隐藏房间黑块遮罩.rb
+++ scripts/隐藏房间黑块遮罩.rb
@@ -1,145 +1,162 @@
 #==============================================================================
-# ■ 隐藏房间黑块 v2.0
-#  by：VIPArcher time：20150211
-#  -- 本脚本来自 http://rm.66rpg.com 使用或转载请保留以上信息。
+# ■ 隐藏房间黑块遮罩
+#  by：VIPArcher [email: VIPArcher@sina.com]
+#  -- 本脚本来自 https://rpg.blue 使用或转载请保留以上信息。
 #==============================================================================
-# 使用说明：
-#  · 指定遮罩块颜色可在设置的第一行注释上<color=Color.new(R,G,B)>
-#  · 在需要用黑块遮罩的地图上建一个事件，事件名为 "Black_Room"并且在该事件里注释上
-#  需要遮罩的矩形范围，一行注释设定一个遮罩块，注释示例参考如下：
-#  · 使用颜色RGB都为12的颜色填充区域ID为和1,2的区域踏入时隐藏该黑色遮罩块
-#     <color=Color.new(12,12,12) regid=[1,2]>
-#  · 使用颜色RGB都为12的颜色填充x坐标10,y坐标12,宽度4格,高度6格的的区域遮罩
-#     <color=Color.new(100,255,255) x=10 y=12 w=4 h=6>
-#  · 使用文件名都为 遮罩_A.png 的图片填充x坐标10,y坐标12,触发隐藏区域ID为3的遮罩
-#     <name=遮罩_A x=10 y=12 regid=[3]>
-#  · 使用脚本默认颜色填充触发隐藏区域ID为8的遮罩 <black regid=[8]>
-#   使用图片做为遮罩块必须设置踏入的矩形。文件放在"\Graphics\Parallaxes"文件夹下
+=begin
+  使用说明：
+   · 在地图上建一个事件，事件名为 "Black_Room_Config" 并且在该事件里注释上
+  <mask=文件名（不包含后缀）>
+    path: BlackRoomFiles
+    color: Color.new(33,33,33)
+    x: 3
+    y: 0
+    z: 250
+    edge_width: 20
+    draw: [3,9,9,13][7,0,9,9]
+    check: [2,8,10,14][8,0,10,10]
+    min_opacity: 75
+    max_opacity: 200
+    switch_id: 5
+  </mask>
+  · 以上的配置都有默认值，可以不配置，注释框可能不够写，可以使用滚动文字、脚本框、
+     显示对话来填，用来设置黑块的事件不会被执行，推荐使用滚动文字配置，不会被换行。
+  · 各字段作用释义：
+       path：使用的素材所在路径，不使用素材绘制则没有意义
+       color：绘制黑块时使用的颜色，使用素材绘制黑块时无效
+       x: 遮罩区域的 x 坐标
+       y：遮罩区域的 y 坐标
+       z: 遮罩块的 z 值（在人物上方：200 同层：100 下方：0，可以做个参考）
+       edge_width：绘制黑块时边缘的留白宽度，使用素材绘制黑块时无效
+       draw：要绘制黑块的范围，[左上角x,左上角y,右下角x,右下角y] 写多个会进行拼接
+       check：检查玩家踏入的范围，在范围内时，黑块将隐藏，设置规则同上
+       min_opacity：隐藏时不透明度最低值 默认0
+       max_opacity：显示时不透明度最大值 默认255
+       switch_id：关联的开关，设置的开关打开时，视为玩家在范围内，将隐藏黑块
+       <mask=图片名称> 标签内的文件名是在使用素材做遮罩时读取的文件名
+  · 注意：
+       使用素材绘制遮罩时：必须指定 x、y、check
+       不用素材绘制遮罩时：x、y会自动计算，无需设置，check 如果未设置则与 draw 一致
+       多个矩形之间允许重叠范围，注意不要换行，看地图编辑器右下角坐标就可以轻松取值
+       Black_Room_Config 的事件可以有多个，一个事件里也可以写多个遮罩
+=end
 #==============================================================================
-$VIPArcherScript ||= {};$VIPArcherScript[:black_room] = 20170913
+$VIPArcherScript ||= {};$VIPArcherScript[:black_room] = 20220520
 #-------------------------------------------------------------------------------
 module VIPArcher end
 #==============================================================================
 # ★ 设定部分 ★
 #==============================================================================
 module VIPArcher::BlackRoom
-  Z = 200                              # 遮罩块的Z值
-  SW = 1                              # 该开关开启时遮罩块不可见
-  Edge = 16                           # 遮罩块边距留空的距离
-  Room_Color = Color.new(0,0,0)       # 默认遮罩块填充的颜色
-  Event_Name = 'Black_Room'           # 设置遮罩块的事件名
-  Opacity_Speed = 16                  # 透明度变化的速度
-  File_Path = "Graphics/Parallaxes/"  # 如果使用图片遮罩图片的路径
+  SW = 1                                         # 该开关开启时遮罩块不可见
+  RE_OUTER   = /<mask[= ]?(.*?)>(.*?)<\/mask>/mi # 读取备注用的正则表达式
+  RE_INNER   = /(\w+) *: *(.*)/                  # 读取设置用的正则表达式
+  DEFAULT_Z  = 300                               # 遮罩块的Z值
+  EDGE_WIDTH = 16                                # 遮罩块边距留空的距离
+  EVENT_NAME = 'Black_Room_Config'               # 设置遮罩块的事件名
+  DEFAULT_COLOR = Color.new(0,0,0)               # 默认遮罩块填充的颜色
+  DEFAULT_PATH  = 'Parallaxes'                   # 素材存放默认文件夹 Graphics 下
+  OPACITY_SPEED = 17                             # 透明度变化的速度
 end
 #==============================================================================
 # ☆ 设定结束 ☆
 #==============================================================================
+
 class Sprite_BlackRoom < Sprite
   include VIPArcher::BlackRoom
-  def initialize(settings, viewport = nil)
+  #--------------------------------------------------------------------------
+  # ● 初始化对象
+  #--------------------------------------------------------------------------
+  def initialize(settings, viewport)
     super(viewport)
-    @settings, self.z, @player = settings, Z, $game_player
-    get_size_pos
-    create_bitmap
-  end
-  #--------------------------------------------------------------------------
-  # ● 设置遮罩图位置大小信息
-  #--------------------------------------------------------------------------
-  def get_size_pos
-    if @settings['rect']
-      r = @settings['rect'].scan(/\d+/).collect(&:to_i)
-      @rect = Rect.new(r[0], r[1], r[2], r[3])
-      self.x, self.y = @rect.x * 32, @rect.y * 32
-      @bitmap_width  = @rect.width  - @rect.x + 1
-      @bitmap_height = @rect.height - @rect.y + 1
-    elsif @settings['regid']
-      @res, @pos = @settings['regid'].scan(/\d+/).collect(&:to_i), []
-      $game_map.data.xsize.times do |x|
-        $game_map.data.ysize.times do |y|
-          @pos.push [x,y] if @res.include?($game_map.data[x, y, 3] >> 8)
-        end
-      end
-      return p "似乎备注了未设置区域ID的遮罩块" if @pos.size.zero?
-      @pos.each do |p|
-        @min_x = p[0] if !@min_x || @min_x > p[0]
-        @min_y = p[1] if !@min_y || @min_y > p[1]
-        @max_x = p[0] if !@max_x || @max_x < p[0]
-        @max_y = p[1] if !@max_y || @max_y < p[1]
-      end
-      @bitmap_width  = (@max_x - @min_x + 1) * 32 + Edge * 2
-      @bitmap_height = (@max_y - @min_y + 1) * 32 + Edge * 2
-      self.x, self.y = (@min_x * 32 - Edge), (@min_y * 32 - Edge)
-    end
-    self.x = @settings['x'].to_i * 32 if @settings['x']
-    self.y = @settings['y'].to_i * 32 if @settings['y']
-    @bitmap_width = @settings['w'].to_i * 32 if @settings['w']
-    @bitmap_height = @settings['h'].to_i * 32 if @settings['h']
-    if @settings['x'] && @settings['h']
-      @rect = Rect.new(
-        @settings['x'].to_i,  @settings['y'].to_i,
-        @settings['x'].to_i + @settings['w'].to_i - 1,
-        @settings['y'].to_i + @settings['h'].to_i - 1
-      )
-    end
-  end
-  #--------------------------------------------------------------------------
-  # ● 创建遮罩图像
-  #--------------------------------------------------------------------------
-  def create_bitmap
-    if @settings['name']
-      self.bitmap = Cache.load_bitmap(File_Path, @settings['name'])
+    @settings = settings
+    @check_rects = []
+    basename = extract(nil)
+    self.z = extract('z', DEFAULT_Z).to_i
+    @switch_id = extract('switch_id', SW).to_i
+    @min_opacity = extract('min_opacity', 0).to_i
+    @max_opacity = extract('max_opacity', 255).to_i
+    @opacity_speed = extract('opacity_speed', OPACITY_SPEED).to_i
+    if basename.empty?
+      return unless extract('draw')
+      draw_rects = make_rects(extract('draw',''))
+      make_bitmap(draw_rects)
+      @check_rects = extract('check') ?
+      make_rects(extract('check','')) : draw_rects
     else
-      self.bitmap = Bitmap.new(@bitmap_width, @bitmap_height)
-      color = @settings['color'] ? eval(@settings['color']) : Room_Color
-      if @pos && !@pos.size.zero?
-        @pos.each do |pos|
-          bitmap.fill_rect(
-            (pos[0] - @min_x) * 32,
-            (pos[1] - @min_y) * 32,
-            32 + Edge * 2, 32 + Edge * 2, color
-          )
-        end
-      else
-        self.bitmap.fill_rect(Edge, Edge, @bitmap_width - Edge * 2,
-          @bitmap_height - Edge * 2, color)
+      folder_name = "Graphics/#{extract('path', DEFAULT_PATH).chomp}"
+      self.bitmap = Bitmap.new("#{folder_name}/#{basename}")
+      if RUBY_VERSION == '1.9.2'
+        p "#{basename}遮罩未设置踏入区域" unless extract('check')
       end
+      @check_rects = make_rects(extract('check',''))
+      self.x = extract('x', 0).to_i * 32
+      self.y = extract('y', 0).to_i * 32
+    end
+    self.opacity = character_pos? ? @min_opacity : @max_opacity
+  end
+  #--------------------------------------------------------------------------
+  # ● 通过备注的设置的矩形绘制 Bitmap
+  #--------------------------------------------------------------------------
+  def make_bitmap(draw_rects)
+    bitmap_x = draw_rects.min_by { |rect| rect.x }.x
+    bitmap_y = draw_rects.min_by { |rect| rect.y }.y
+    bitmap_width = draw_rects.max_by { |rect| rect.width }.width
+    bitmap_height = draw_rects.max_by { |rect| rect.height }.height
+    self.x = bitmap_x * 32
+    self.y = bitmap_y * 32
+    self.bitmap = Bitmap.new(bitmap_width * 32, bitmap_height * 32)
+    color = extract('color', DEFAULT_COLOR)
+    edge_width = extract('edge_width', EDGE_WIDTH).to_i
+    draw_rects.each do |rect|
+      fill_rect = Rect.new(
+        edge_width + (rect.x - bitmap_x) * 32,
+        edge_width + (rect.y - bitmap_y) * 32,
+        (rect.width - rect.x + 1) * 32 - edge_width * 2,
+        (rect.height - rect.y + 1) * 32 - edge_width * 2
+      )
+      self.bitmap.fill_rect(fill_rect, color)
     end
   end
   #--------------------------------------------------------------------------
-  # ● 释放
+  # ● 备注字符串转矩形数组
   #--------------------------------------------------------------------------
-  def dispose
-    super
-    self.bitmap.dispose
+  def make_rects(str)
+    str.scan(/\[[\d, ]+?\]/mi).collect do |rect|
+      eval("Rect.new(#{$1})") if rect =~ /\[(\d+.*?)\]/
+    end
   end
   #--------------------------------------------------------------------------
-  # ● 位置判定
+  # ● 提取备注中的设定值
+  #--------------------------------------------------------------------------
+  def extract(key, default = nil)
+    return eval(@settings[key]) if key == 'color' && @settings[key]
+    @settings[key] || default
+  end
+  #--------------------------------------------------------------------------
+  # ● 检查位置是否在设定的范围内
   #--------------------------------------------------------------------------
   def character_pos?
-    if @rect
-      @player.x > @rect.width  || @player.x <  @rect.x ||
-      @player.y > @rect.height || @player.y <  @rect.y
-    elsif @res
-      !@res.include?($game_map.region_id(@player.x, @player.y + 1)) &&
-      !@res.include?($game_map.region_id(@player.x - 1, @player.y)) &&
-      !@res.include?($game_map.region_id(@player.x + 1, @player.y)) &&
-      !@res.include?($game_map.region_id(@player.x, @player.y - 1)) &&
-      !@res.include?($game_map.region_id(@player.x,     @player.y))
-    end
+    @check_rects.any? do |rect|
+      $game_player.x <= rect.width  && $game_player.x >= rect.x &&
+      $game_player.y <= rect.height && $game_player.y >= rect.y
+    end || $game_switches[@switch_id]
   end
   #--------------------------------------------------------------------------
   # ● 更新透明度
   #--------------------------------------------------------------------------
   def update_opacity
-    self.opacity += character_pos? ? Opacity_Speed : - Opacity_Speed
-    self.opacity = 0 if $game_switches[SW]
+    self.opacity = character_pos? ?
+    [self.opacity - @opacity_speed, @min_opacity].max :
+    [self.opacity + @opacity_speed, @max_opacity].min
+    self.visible = !$game_switches[SW]
   end
   #--------------------------------------------------------------------------
-  # ● 更新位置
+  # ● 更新地图卷动位置
   #--------------------------------------------------------------------------
   def update_position
-    self.ox = $game_map.display_x * 32
-    self.oy = $game_map.display_y * 32
+    self.ox = $game_map.display_x * (RUBY_VERSION == '1.9.2' ? 32 : 0.125)
+    self.oy = $game_map.display_y * (RUBY_VERSION == '1.9.2' ? 32 : 0.125)
   end
   #--------------------------------------------------------------------------
   # ● 更新画面
@@ -149,63 +166,70 @@ class Sprite_BlackRoom < Sprite
     update_opacity
     update_position
   end
+  #--------------------------------------------------------------------------
+  # ● 释放
+  #--------------------------------------------------------------------------
+  def dispose
+    self.bitmap.dispose
+    super
+  end
 end
 #-------------------------------------------------------------------------------
 class Spriteset_Map
   include VIPArcher::BlackRoom
+  # 显示文字：（401） 滚动文本：（405） 注释：(108, 408) 脚本：（355, 655）
+  EVENT_CODE = [108, 408, 405, 355, 655, 401] # 可以进行填写配置的事件类型编号
   #--------------------------------------------------------------------------
-  # ● 生成黑色遮罩块
+  # ● 生成人物精灵
   #--------------------------------------------------------------------------
   alias black_room_create_characters create_characters
   def create_characters
+    old_map_id = @map_id # 兼容部分脚本 by：KB.Driver（日历的付丧神）
     black_room_create_characters
-    room_sprite_dispose if @black_room_sprites
+    old_map_id != @map_id ? room_sprite_dispose : return if @black_room_sprites
+    create_black_rooms
+  end
+  #--------------------------------------------------------------------------
+  # ● 生成黑色遮罩块
+  #--------------------------------------------------------------------------
+  def create_black_rooms
     @black_room_sprites = []
     $game_map.events.values.each do |event|
-      if event.instance_variable_get(:@event).name =~ /#{Event_Name}/i
-        event.list.each do |command|
-          get_black_room_setup(command) if [108, 408].include?(command.code)
-        end if event.list
+      if event.instance_variable_get(:@event).name =~ /#{EVENT_NAME}/i
+        return if event.list.nil?
+        event.list.unshift(
+          RPG::EventCommand.new(115)
+        ) if event.list.first.code != 115
+        event.list.inject('') do |result, command|
+          next result unless EVENT_CODE.include?(command.code)
+          "#{result}#{command.parameters.join()}\n"
+        end.scan(RE_OUTER).map do |name, contents|
+          settings = {nil => name}
+          contents.scan(RE_INNER) do |key, value|
+            (settings[key] ||= '') << value
+          end
+          sprite = Sprite_BlackRoom.new(settings, @viewport1)
+          @black_room_sprites.push(sprite)
+        end
       end
     end
   end
   #--------------------------------------------------------------------------
-  # ● 获取遮罩块设置
-  #--------------------------------------------------------------------------
-  def get_black_room_setup(command)
-    command.parameters.each do |line|
-      settings = {}
-      if line =~ /<name *= *(.*?) +(.*?)>/i
-        settings['name'] = $1
-      elsif line =~ /<color *= *(.*?) +(.*?)>/i
-        settings['color'] = $1
-      else
-        line =~ /<black( +)(.*?)>/i
-      end
-      get_settings(settings, $2)
-    end
-  end
-  #--------------------------------------------------------------------------
-  # ● 解析备注生成遮罩块
-  #--------------------------------------------------------------------------
-  def get_settings(settings, opt)
-    opt.scan(/(\w+) *= *([\d\[,\]]*)/) do |key, value|
-      (settings[key] ||= '') << value
-    end
-    sprite = Sprite_BlackRoom.new(settings, @viewport1)
-    sprite.opacity = 0 unless sprite.character_pos?
-    @black_room_sprites.push(sprite)
-  end
-  #--------------------------------------------------------------------------
-  # ● 释放遮罩块
+  # ● 释放
   #--------------------------------------------------------------------------
   alias black_room_dispose dispose
   def dispose
     black_room_dispose
+    room_sprite_dispose
+  end
+  #--------------------------------------------------------------------------
+  # ● 释放遮罩块
+  #--------------------------------------------------------------------------
+  def room_sprite_dispose
     @black_room_sprites.each {|sprite| sprite.dispose }
   end
   #--------------------------------------------------------------------------
-  # ● 更新遮罩块
+  # ● 更新画面
   #--------------------------------------------------------------------------
   alias black_room_update update
   def update
